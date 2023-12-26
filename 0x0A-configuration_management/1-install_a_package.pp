5f6c25df40bd7510c3f01006db13af1d89434e8a
oussama amahrouch <oussama_98@hotmail.fr>
2023-12-26 06:38:46 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install Flask 2.1.0
-package { 'Flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
+# Install Flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
