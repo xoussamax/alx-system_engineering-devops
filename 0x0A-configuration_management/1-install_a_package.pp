c8fadf7c2955afe35e90ee9652217f0dca63fb6f
oussama amahrouch <oussama_98@hotmail.fr>
2023-12-27 10:44:04 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install a package
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Using Puppet, install flask from pip3.
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
 }
