ec47ef985edd6eb4bee26e327b42bafa6374e75e
oussama amahrouch <oussama_98@hotmail.fr>
2023-12-31 08:59:41 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Make changes to configuration file using Puppet
 include stdlib
 
-file_line { 'Refuse to authenticate using a password':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line { 'Use private key':
+file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
