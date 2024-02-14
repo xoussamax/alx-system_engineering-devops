759b57e60bc804b36923bee67109d28f56dbf499
oussama amahrouch <oussama_98@hotmail.fr>
2024-02-14 19:01:09 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,49 +1,14 @@
 #!/usr/bin/python3
 """number of subscribers for a given subreddit"""
-from typing import Optional
-import json
-from urllib.parse import quote # For URL encoding
+def number_of_subscribers(subreddit):
+    """Queries Reddit API and returns the
+    number of subscribers for subreddit"""
+    from requests import get
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = {'User-agent': 'User-agent'}
 
-import requests
+    response = get(api_url, headers=user_agent, allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
 
-API_URL = "http://www.reddit.com/"
-USER_AGENT = {"User-agent": "Mozilla/5.0"}
-CONTENT_TYPE = "application/json; charset=utf-8"
-
-def number_of_subscribers(subreddit: str) -> int:
-    """Returns the number of subscribers for a given subreddit."""
-    try:
-        about_page = _fetch_about_page(subreddit)
-        
-        # Checking for correct content type
-        assert CONTENT_TYPE in about_page.headers['Content-Type']
-    
-        page_content = about_page.text
-            
-            # Parsing JSON data
-            parsed_dict = json.loads(page_content)
-                
-                # Extracting subscriber count
-                num_subscribers = parsed_dict['data']['subscribers']
-                
-                return num_subscribers
-    except Exception:
-        raise ValueError(f"Invalid subreddit '{subreddit}'!")
-
-def _fetch_about_page(subreddit: str) -> Optional[requests.Response]:
-    """Fetches the /about.json endpoint for a given subreddit."""
-    encoded_sr = quote(subreddit)  
-    target_endpoint = f"/r/{encoded_sr}/about.json"
-    full_target_path = API_URL + target_endpoint
-    
-    resp = requests.get(full_target_path, headers=USER_AGENT)
-    
-    # Checking for possible redirections
-    if resp.history:
-        last_resp = resp.history[-1]
-        location = last_resp.headers['Location']
-        if '/search?q=' in location:
-            # We were redirected to search results => non-existent subreddit
-            return None
-    
-    return resp
+    return response.json().get("data").get("subscribers")
