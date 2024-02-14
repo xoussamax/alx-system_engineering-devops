dbb7281a5def5ec0dc4ac0b06172ccf0df6e77e3
oussama amahrouch <oussama_98@hotmail.fr>
2024-02-14 18:05:37 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-""" ahaa"""
-from requests import get
-import json
+'''Function that queries the Reddit API and returns the number of subscribers
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
 
 def number_of_subscribers(subreddit):
-    # Check if input is a string
-    if not isinstance(subreddit, str):
-        raise TypeError("Subreddit must be a string")
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
-    headers = {"User-Agent": "my_app"}  # Set a custom user agent to avoid rate limiting issues
-
-    try:
-        response = get(url, headers=headers)
-        
-        if response.status_code != 200:
-            return 0
-            
-        data = json.loads(response.text)
-        subscriber_count = int(data["data"]["subscribers"])
-        return subscriber_count
-
-    except Exception as e:
-        print(f"An error occurred while fetching {subreddit}: {str(e)}")
-        return 0
+    '''returns  the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
