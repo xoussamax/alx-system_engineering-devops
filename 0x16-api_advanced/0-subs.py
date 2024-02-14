b8054616163c57b51aa1aa8d616ee7f0ef4ba2c0
oussama amahrouch <oussama_98@hotmail.fr>
2024-02-14 19:02:04 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """number of subscribers for a given subreddit"""
+
+
 def number_of_subscribers(subreddit):
     """Queries Reddit API and returns the
     number of subscribers for subreddit"""
     from requests import get
+
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     user_agent = {'User-agent': 'User-agent'}
 
